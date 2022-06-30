e673b4c7b6140ec47d8f9128f55998e8d1d77089
Gabriel Boudreaux <boudreauxgabriel1@gmail.com>
2022-06-30 08:49:25 -0500

    commit tree 78d6a0cfcd8e87aa692453d2856a7739ac418db9

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", updateDOM );
+
+function updateDOM() {
+  document.getElementById( "text" ).innerHTML = "This is really cool!";
+}
